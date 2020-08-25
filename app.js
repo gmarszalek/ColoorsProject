12f2060169a331b663111888ded4a0f25853a118
gmarszalek <grzegorzmarszalek92@gmail.com>
2020-08-25 20:37:25 +0200

    create color generator with chroma-js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const colorDivs = document.querySelectorAll(".color");
 const generateBtn = document.querySelector(".generate");
 const sliders = document.querySelectorAll('input[type="range"]');
 const currentHexes = document.querySelectorAll(".color h2");
-let initialColors;
 
 //Functions
 function generateHex() {
