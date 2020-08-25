30e91e3e8e1e4cd14a25177cb9c909152ff59c8b
gmarszalek <grzegorzmarszalek92@gmail.com>
2020-08-25 20:34:52 +0200

    git commit -m

--- app.js
+++ app.js
@@ -19,7 +19,18 @@ function randomColors() {
     //add the color to the background
     div.style.backgroundColor = randomColor;
     hexText.innerText = randomColor;
+    //Check for contrast
+    checkTextContrast(randomColor, hexText);
   });
+}
+
+function checkTextContrast(color, text) {
+  const luminance = chroma(color).luminance();
+  if (luminance > 0.5) {
+    text.style.color = "black";
+  } else {
+    text.style.color = "white";
+  }
 }
 
 randomColors();
